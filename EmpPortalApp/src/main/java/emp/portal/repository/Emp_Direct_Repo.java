141f7731ad3c57d6fcfbab206a6b4fa5e508ff63
mohd-ahad <99248114+mohd-ahad@users.noreply.github.com>
2022-02-24 15:42:22 +0530

    Add files via upload

---
+++ EmpPortalApp/src/main/java/emp/portal/repository/Emp_Direct_Repo.java
@@ -0,0 +1,27 @@
+package emp.portal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import emp.portal.entity.Employee;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface Emp_Direct_Repo  extends JpaRepository<Employee, Long> {
+Optional<Employee> findByName(String name);
+@Query(value="SELECT * FROM employee e WHERE e.Emp_Name LIKE %?1%"+
+" OR e.Account_Mapped LIKE %?1%",
+nativeQuery=true)
+List<Employee> findAll(String name);
+List<Employee> findAllByNameContainingIgnoreCase(String name);
+
+	//public List<Employee> findByAccount(String account);
+//	@Query("from Employee as e where e.id=:id")
+//	public Page<Employee> findEmployeeById(@Param("id")int id,Pageable pageable);
+//	List<Employee> findAll(String accountName);
+	
+}
